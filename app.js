45358155e75f64b2c2a518dea21a59eca6a87542
unknown <buzzer79@gmail.com>
2019-05-20 22:10:43 +0200

    core status

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+
+require('dotenv').config();
+
+const appRoutes = require("./routes/routes");
+
+const app = express();
+app.use(helmet());
+
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.static("public"));
+
+app.set("view engine","ejs");
+
+
+
+app.use(appRoutes);
+
+app.listen(port);
+
