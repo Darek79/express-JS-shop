09841275ca42b5929c708956f240822e6b76c906
unknown <buzzer79@gmail.com>
2019-06-28 00:38:59 +0200

    cart done

--- public/js/productPage.js
+++ public/js/productPage.js
@@ -1,40 +1,46 @@
+
 const btn = document.querySelectorAll(".add");
 const cN = document.getElementById("cartNr");
 const cart = document.getElementById("cart");
 
-let t,p,i;
-let res;
+let t, p, i,resData;
 
-btn.forEach((val)=>{
-val.addEventListener("click",(e)=>{
 
-    e.preventDefault();
-     t = val.getAttribute("data-title");
-     p = val.getAttribute("data-price");
-     i = val.getAttribute("data-img");
+btn.forEach((val) => {
+    val.addEventListener("click", async(e) => {
 
-     fetch("/addToCart",{
-         method: "POST",
-         body: JSON.stringify({
-             t,
-             p,
-             i
-         }),
-         headers:{
-             "Content-Type": "application/json"
-         }
-     })
-     .then(res => res.json())
-     .then((resData) => {
-        cN.innerHTML = resData.cartFiles.length; 
-        res = Promise.resolve(resData.cartFiles);
+        e.preventDefault();
+        t = val.getAttribute("data-title");
+        p = val.getAttribute("data-price");
+        i = val.getAttribute("data-img");
+     
+         const resp = await fetch("/addToCart",{
+                method: "POST",
+                body: JSON.stringify({
+                    t,
+                    p,
+                    i
+                }),
+                headers:{
+                    "Content-Type": "application/json"
+                }
+            })
+            resData = await resp.json();
+            cN.innerHTML = resData.cartFiles.length;
+            
+
+
         })
-     .catch(err => console.log(err));
 
-})
 
-});
+    });
 
-cart.addEventListener("click",()=>{
-    console.log(res);
+cart.addEventListener("click", async() => {
+    const resp = await fetch("/getCart");
+    const resData = await resp.json();
+    console.log(resData.cartJSON);
+    
+    resData.cartJSON.forEach((val)=>{
+        console.log(val);
+    })
 });
--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,19 @@ const imgDisplay = require("../controllers/frontPagePics");
 const prodPageC = require("../controllers/productPageC");
 const prodPageB = require("../controllers/productPageB");
 const prod = require("../controllers/cart");
+const readCart = require("../controllers/getCartData");
 
 router.get("/",imgDisplay.loadFrontPics);
 
 router.get("/prodPageB/:title/:ind",prodPageB.productPageB);
 router.get("/prodPageC/:title/:ind",prodPageC.productPageC);
 router.post("/addToCart",prod.addToCart);
+router.get("/getCart",async(req,res)=>{
+    const cartJSON = await readCart.cartData();
+    res.status(200).json({
+        cartJSON
+    })
+})
 
 router.use((req,res)=>{
     res.status(404).render("error");
