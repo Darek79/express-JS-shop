7e5e7083fb38aebd90efebf76df74337354a48ce
unknown <buzzer79@gmail.com>
2019-06-30 23:29:41 +0200

    done

--- controllers/sidebar.js
+++ controllers/sidebar.js
@@ -2,9 +2,12 @@ const sideData = require("../katalog/sidebar.json");
 
 exports.getSidebarData = async(req,res)=>{
     const categ = await req.body.cat;
+    const def = await req.body.def;
+
+    let data = def === "def"?sideData.damen:sideData[categ];
     
     res.status(200).json({
-        dataItem: sideData[categ]
+        dataItem: data
     })
 
 }
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,6 +1,25 @@
 const btn = document.querySelectorAll(".sCat");
 const mainDiv = document.getElementById("topics");
+const ham = document.getElementById("ham");
 console.log(btn);
+
+ham.addEventListener("click",async()=>{
+    const resData = await fetch("/sidebarData",{
+        method : "POST",
+        body: JSON.stringify({
+            def: "def"
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const resCat = await resData.json();
+    console.log(resCat);
+    const img = encodeURI(resCat.dataItem[0].backImg);
+    mainDiv.style.backgroundImage = `url(${img})`
+    
+})
+
 
 btn.forEach((val)=>{
     val.addEventListener("click",async(e)=>{
@@ -16,9 +35,10 @@ btn.forEach((val)=>{
             }
         })
         const resCat = await resData.json();
+        console.log(resCat);
         const img = encodeURI(resCat.dataItem[0].backImg);
         mainDiv.style.backgroundImage = `url(${img})`
 
-        
+
     })
 })
